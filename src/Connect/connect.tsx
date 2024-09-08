da6be5ed8bce88c3f8e7ca3c3f50f1a56d8a6cb0
ADILRAQ <raqiouiadil852@gmail.com>
2024-09-08 10:51:37 +0100

    MOD: Adding link in the Connect buttons

--- src/Connect/connect.tsx
+++ src/Connect/connect.tsx
@@ -1,20 +1,19 @@
 import * as React from "react";
 import './connect.css';
 
-// FIXME make more good UI for those buttons
-
 const Connect = () => {
-
-    const handleClick = (id :number) => {
-        // TODO: handle click logic
-        console.log(id);
-    }
 
     return (
         <div className="connect--holder">
-            <div className="btn link dosis-light" onClick={() => handleClick(1)} >LINKEDIN</div>
-            <div className="btn resume dosis-light" onClick={() => handleClick(2)} >RESUME</div>
-            <div className="btn git dosis-light" onClick={() => handleClick(3)} >GITHUB</div>
+            <a className="btn link dosis-light" href="https://www.linkedin.com/in/adil-raqioui" target="to_blank">
+                LINKEDIN
+            </a>
+            <a className="btn resume dosis-light" href="resume.pdf" target="to_blank">
+                RESUME
+            </a>
+            <a className="btn git dosis-light" href="https://github.com/ADILRAQ" target="to_blank">
+                GITHUB
+            </a>
         </div>
     );
 }
