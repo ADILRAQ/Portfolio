8147a1aae13706885729233f7bf232ce2cf0af6c
ADILRAQ <raqiouiadil852@gmail.com>
2024-10-17 11:44:26 +0100

    ADD: creating a Button story

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 const Navbar = () => {
     return (
         <div className="right-4 h-16 flex gap-5">
-            <Button onClick={() => ''} variant='styled' sz="sm" >Projects</Button>
+            <Button onClick={() => ''} variant='normal' sz="sm" >Projects</Button>
             <Button onClick={() => ''} variant='styled' sz="md" >Articles</Button>
             <Button onClick={() => ''} variant='styled' sz="lg" >Resume {'>'}</Button>
             <Button onClick={() => ''} variant='dark' sz="lg" >Resume {'>'}</Button>
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,4 +1,52 @@
-// import Button from '../components/Button';
+import { Meta, StoryObj } from '@storybook/react';
+import Button from "../components/Button";
+import { ComponentProps } from 'react';
 
-// type 
-export {};
+type StoryProps = ComponentProps<typeof Button>;
+
+const meta: Meta<StoryProps> = {
+    component: Button,
+}
+
+export default meta;
+
+type Story = StoryObj<StoryProps>;
+
+export const Normal :Story = {
+    args: {
+        variant: 'normal',
+        sz: "sm",
+        children: 'Projects'
+    },
+    // render: (args) => {
+    //     return (
+    //         <Button {...args}>{'Resume >'}</Button>
+    //     );
+    // }
+};
+
+export const Styled :Story = {
+    args: {
+        variant: 'styled',
+        sz: 'md',
+        children: 'Projects'
+    },
+    // render: (args) => {
+    //     return (
+    //         <Button {...args}>{'Resume >'}</Button>
+    //     );
+    // }
+};
+
+export const Dark :Story = {
+    args: {
+        variant: 'dark',
+        sz: 'md',
+        children: 'Projects'
+    },
+    // render: (args) => {
+    //     return (
+    //         <Button {...args}>{'Resume >'}</Button>
+    //     );
+    // }
+};
