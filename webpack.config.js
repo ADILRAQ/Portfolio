046bc6413dbc1df7d63f27af2dedee114aec02e0
ADILRAQ <raqiouiadil852@gmail.com>
2024-10-25 16:24:38 +0100

    MOD: Modifying webpack.config to add loaders for tailwind

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,12 @@ const isProduction = process.env.NODE_ENV == 'production';
 
 
 const config = {
-    entry: './src/index.ts',
+    entry: './src/index.tsx',
     output: {
         path: path.resolve(__dirname, 'dist'),
     },
     devServer: {
-        open: false,
+        open: true,
     },
     plugins: [
         // Add your plugins here
@@ -23,10 +23,23 @@ const config = {
                 test: /\.(ts|tsx)$/i,
                 loader: 'ts-loader',
                 exclude: ['/node_modules/'],
+                options: {
+                    compilerOptions: {
+                        noEmit: false, // this option will solve the issue 
+                    },
+                }
             },
             {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
                 type: 'asset',
+            },
+            {
+                test: /\.css$/,
+                use: [
+                  'style-loader', // Injects styles into the DOM
+                  'css-loader', // Turns CSS into CommonJS
+                  'postcss-loader', // Processes CSS with PostCSS
+                ],
             },
 
             // Add your rules for custom modules here
